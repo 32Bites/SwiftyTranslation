aa4318cf4b4482c257095d9a35cabbcf4a005214
Noah Scott <noah@thenoah.party>
2019-07-12 13:44:06 -0700

    Bug fixes, and language detection!
    Fixes a url encoding bug, and is able to detect a text's language.

--- Sources/SwiftyTranslation/SwiftyTranslation.swift
+++ Sources/SwiftyTranslation/SwiftyTranslation.swift
@@ -1,38 +1,40 @@
+//
+//  TranslatorEngine.swift
+//  Just
+//
+//  Created by Noah Scott on 7/11/19.
+//
+
 import Foundation
 
 public struct SwiftyTranslation {
     /**
-     The class for the google translator
+     The class for the Google Translator Engine
      */
     public class TranslatorEngine {
         var apiKey : String?
-        var projectId : String?
         
         /**
          Creates a TranslatorEngine object for translation.
          */
-        public init(apiKey: String?, projectId : String?) throws {
+        public init(apiKey: String?) throws {
             if apiKey == nil {
                 throw TranslatorEngineInitalizationError.apiKeyNil
-            } else if projectId == nil {
-                throw TranslatorEngineInitalizationError.projectIdNil
             }
             self.apiKey = apiKey
-            self.projectId = projectId
         }
     }
     
     public enum API: String {
         case translate = "https://translation.googleapis.com/language/translate/v2?q={{TEXT}}&target={{TARGET}}&key={{KEY}}"
-        case detect = "Detect"
+        case detect = "https://translation.googleapis.com/language/translate/v2/detect?q={{TEXT}}&key={{KEY}}"
         case supportedLangs = "Supported Languages"
     }
     
     enum TranslatorEngineInitalizationError: Error {
         case apiKeyNil
-        case projectIdNil
     }
-    enum translationError: Error {
-        case responseTextNil
+    enum networkError: Error {
+        case responseNil
     }
 }
--- Sources/SwiftyTranslation/TranslatorEngine.swift
+++ Sources/SwiftyTranslation/TranslatorEngine.swift
@@ -11,20 +11,43 @@ import SwiftyJSON
 
 extension SwiftyTranslation.TranslatorEngine {
     /**
-     Translate text to another language. Use the ISO-639-1 Code: https://cloud.google.com/translate/docs/languages
+     Translate text to another language. Use an ISO-639-1 Code. See: https://cloud.google.com/translate/docs/languages
+     
+     - parameter outputLanguage: The language to translate your text to.
+     - parameter inputText: The text to translate
      */
-    /// - parameter outputLanguage: The language to translate your text to
-    /// - parameter text: The text to translate
-    public func translate(outputLanguage: String, text: String) throws -> [String : String] {
+    public func translate(outputLanguage: String, inputText: String) throws -> [String : String] {
         var translation : [String : String] = [:]
+        let text = inputText.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed)!
         let url = SwiftyTranslation.API.translate.rawValue.replacingOccurrences(of: "{{TEXT}}", with: text).replacingOccurrences(of: "{{TARGET}}", with: outputLanguage).replacingOccurrences(of: "{{KEY}}", with: self.apiKey!)
         let httpRequest = Just.get(url)
-        if httpRequest.text == nil {
-            throw SwiftyTranslation.translationError.responseTextNil
-        } else {
-            let jsonData = JSON(httpRequest.content!)
-            translation = ["detectedLanguage" : jsonData["data"].dictionaryValue["translations"]![0].dictionaryValue["detectedSourceLanguage"]!.stringValue, "translation": jsonData["data"].dictionaryValue["translations"]![0].dictionaryValue["translatedText"]!.stringValue]
+        if httpRequest.content == nil {
+            throw SwiftyTranslation.networkError.responseNil
         }
+        let jsonData = JSON(httpRequest.content!)
+        let detectedLanguage = jsonData["data"].dictionaryValue["translations"]![0].dictionaryValue["detectedSourceLanguage"]!.stringValue
+        let translatedText = jsonData["data"].dictionaryValue["translations"]![0].dictionaryValue["translatedText"]!.stringValue
+        translation = ["detectedLanguage" : detectedLanguage, "translatedText": translatedText]
         return translation
+    }
+    
+    
+    /**
+     Detect the language of a string. Outputs an ISO-639-1 Code. See: https://cloud.google.com/translate/docs/languages
+     
+     - parameter inputText: The text to detected the language for.
+     */
+    public func detect(inputText: String) throws -> [String : Any] {
+        let text = inputText.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed)!
+        let url = SwiftyTranslation.API.detect.rawValue.replacingOccurrences(of: "{{TEXT}}", with: text).replacingOccurrences(of: "{{KEY}}", with: self.apiKey!)
+        let httpRequest = Just.get(url)
+        if httpRequest.content == nil {
+            throw SwiftyTranslation.networkError.responseNil
+        }
+        let jsonData = JSON(httpRequest.content!)
+        let detectedLanguage = jsonData["data"].dictionaryValue["detections"]!.arrayValue[0].arrayValue[0].dictionaryValue["language"]!.stringValue
+        let isReliable = jsonData["data"].dictionaryValue["detections"]!.arrayValue[0].arrayValue[0].dictionaryValue["isReliable"]!.boolValue
+        let confidence = jsonData["data"].dictionaryValue["detections"]!.arrayValue[0].arrayValue[0].dictionaryValue["confidence"]!.doubleValue
+        return ["detectedLanguage" : detectedLanguage, "isReliable" : isReliable, "confidence" : confidence]
     }
 }
